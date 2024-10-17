f6c56ce392a92c95fef8fb449907a8d0ba0f1013
Mohin Uddin <mdmohinuddin4721@gmail.com>
2024-10-17 11:24:45 +0600

    implement error handling

--- scirpt.js
+++ scirpt.js
@@ -5,28 +5,34 @@ let heatIndex = document.querySelector(".heat-index");
 let wind = document.querySelector(".wind");
 let isDay = document.querySelector(".is-day");
 let feelsLike = document.querySelector(".feels-like");
-let btn = document.querySelector('.btn');
+let city = document.querySelector(".city");
+let desc = document.querySelector(".desc");
+let err = document.querySelector(".err")
+let btn = document.querySelector(".btn");
 
 
 const api_url = "https://api.weatherapi.com/v1/"
 const api_key = "77a42dbe007042c8864124951241610"
 
 
-document.querySelector('.form').addEventListener('submit', (event) => {
+document.querySelector(".form").addEventListener("submit", (event) => {
     event.preventDefault();
-    const city = document.querySelector('.inp').value;
+    let city = document.querySelector(".inp").value;
     console.log(city);
-
-
+    
     getInfo(city).then(result => {
         temp.innerText = result.temp;
         humidity.innerText = result.humidity;
         heatIndex.innerText = result.heatIndex;
         wind.innerText = result.wind;
-        isDay.innerText = result.isDay === 0 ? "It's nighttime!" : "It's daytime!";
+        isDay.innerText = result.isDay === 0 ? "nighttime!" : "daytime!";
         feelsLike.innerText = result.feelsLike;
+        city.innerText = result.city;
+        desc.innerText = result.desc;
+        err.innerText = "";
+    }).catch(error =>{
+        err.innerText = "Please enter a city name";  
     })
-
 });
 
 
@@ -42,15 +48,12 @@ let getInfo = async (city) => {
             wind: jsonRes.current.wind_kph,
             isDay: jsonRes.current.is_day,
             feelsLike: jsonRes.current.feelslike_c,
-            name: jsonRes.location.name,
+            city: jsonRes.location.name,
+            desc: jsonRes.current.condition.text,
         }
         console.log(result);
-
         return result;
-
-    } catch (err) {
-       return "Please enter a city name."; 
-
+    } catch (error) {
+        throw error;
     }
-
 }
